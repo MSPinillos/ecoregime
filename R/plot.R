b8512beffbccbd07564796ed8a5ced02c21c7d06
Martina Sánchez-Pinillos <martina.sanchezpinillos@gmail.com>
2023-07-11 10:21:15 -0400

    Improve code and remove the dependency on auxiliary functions

--- R/plot.R
+++ R/plot.R
@@ -44,20 +44,9 @@
 #' Borg, I., & Groenen, P. J. F. (2005). Modern Multidimensional Scaling (2nd ed.).
 #' Springer.
 #'
-#' De Cáceres, M, Coll L, Legendre P, Allen RB, Wiser SK, Fortin MJ, Condit R &
-#' Hubbell S. (2019). Trajectory analysis in community ecology. Ecological
-#' Monographs.
-#'
 #' Sánchez-Pinillos, M., Kéfi, S., De Cáceres, M., Dakos, V. 2023. Ecological Dynamic
 #' Regimes: Identification, characterization, and comparison. *Ecological Monographs*
 #' <https://doi.org/10.1002/ecm.####>
-#'
-#'
-#' @note
-#' This function uses a modified version of [ecotraj::trajectoryPlot()] (v.0.0.3;
-#' De Cáceres et al. 2019). The modification was done the 2022-12-08. It allows
-#' using the function [shape::Arrows()] to plot ecological trajectories instead
-#' of [graphics::arrows()] and setting the parameters of generic [plot()].
 #'
 #' @seealso
 #' [`retra_edr()`] for identifying representative trajectories in EDRs applying
@@ -187,7 +176,7 @@ plot.RETRA <- function (x, d, trajectories, states, select_RT = NULL,
 
   # STATE COORDINATES ----------------------------------------------------------
 
-    # Coordinates MDS
+  # Coordinates MDS
   if (inherits(d, "dist") || isSymmetric(as.matrix(d))) {
     warning(cat("Representative trajectories will be displayed in an ordination space generated through multidimensional scaling (MDS). You can avoid this step by providing state coordinates in the 'd' argument.", "\n"))
     statesMDS <- data.frame(smacof::mds(delta = d, ndim = ncol(d)-1,
@@ -197,6 +186,9 @@ plot.RETRA <- function (x, d, trajectories, states, select_RT = NULL,
     statesMDS <- data.frame(d)
   }
   statesMDS$ID <- paste0(trajectories, "_", states)
+  statesMDS.ls <- lapply(unique(trajectories), function(itraj){
+    statesMDS[which(trajectories == itraj), ]
+  })
 
   # RT data: ID_plot, RT_ID
   RT_data <- data.frame(ID = paste0(RT_traj, "_", RT_states), RT_traj = RT_traj, RT_states = RT_states,
@@ -264,8 +256,21 @@ plot.RETRA <- function (x, d, trajectories, states, select_RT = NULL,
   if (is.null(traj.colors)) {
     traj.colors = rep("grey", length(unique(trajectories)))
   }
-  trajectoryPlot2(d = statesMDS[, -ncol(statesMDS)], sites = trajectories,
-                  surveys = states, traj.colors = traj.colors, axes = axes, ...)
+
+  # Plot individual trajectories in the EDR
+  plot(statesMDS[, axes], type = "n",
+       xlab = paste0("Axis ", axes[1]),
+       ylab = paste0("Axis ", axes[2]), ... )
+  lapply(seq_along(statesMDS.ls), function(itraj){
+    istate = 1
+    while (istate < nrow(statesMDS.ls[[itraj]])) {
+      shape::Arrows(x0 = statesMDS.ls[[itraj]][istate, axes[1]], y0 = statesMDS.ls[[itraj]][istate, axes[2]],
+                    x1 = statesMDS.ls[[itraj]][istate + 1, axes[1]], y1 = statesMDS.ls[[itraj]][istate + 1, axes[2]],
+                    col = traj.colors[itraj], arr.adj = 1)
+      istate = istate + 1
+    }
+  })
+
 
   # Plot segments
   if (is.null(RT.colors)) {
@@ -339,49 +344,3 @@ plot.RETRA <- function (x, d, trajectories, states, select_RT = NULL,
     }
   }
 }
-
-
-
-# Function to plot individual trajectories. This is a modified version of the
-# function trajectoryPlot() in 'ecotraj' Version 0.0.3 (De Cáceres et al. 2019).
-# The modification allows plotting ecological trajectories as arrows using the
-# Arrows() function in 'shape', instead of the arrows() function in base.
-trajectoryPlot2 <- function (d, sites, surveys = NULL, selection = NULL, traj.colors = NULL, traj.lwd = NULL,
-                             axes = c(1, 2), survey.labels = FALSE, ...) {
-  siteIDs = unique(sites)
-  nsite = length(siteIDs)
-  if (is.null(selection))
-    selection = 1:nsite
-  else {
-    if (is.character(selection))
-      selection = (siteIDs %in% selection)
-  }
-  selIDs = siteIDs[selection]
-  xp = d[sites %in% selIDs, axes[1]]
-  yp <- d[sites %in% selIDs, axes[2]]
-  plot(xp, yp, type = "n", xlab = paste0("Axis ", axes[1]),
-       ylab = paste0("Axis ", axes[2]), ...)
-  sitesred = sites[sites %in% selIDs]
-  if (!is.null(surveys))
-    surveysred = surveys[sites %in% selIDs]
-  else surveysred = NULL
-  for (i in 1:length(selIDs)) {
-    ind_surv = which(sitesred == selIDs[i])
-    if (!is.null(surveysred))
-      ind_surv = ind_surv[order(surveysred[sitesred ==
-                                             selIDs[i]])]
-    for (t in 1:(length(ind_surv) - 1)) {
-      niini = ind_surv[t]
-      nifin = ind_surv[t + 1]
-      if (!is.null(traj.colors) & is.null(traj.lwd)){
-        shape::Arrows(xp[niini], yp[niini], xp[nifin], yp[nifin],
-                      col = traj.colors[i], arr.adj = 1, ...)
-      } else if (!is.null(traj.colors) & !is.null(traj.lwd)){
-        shape::Arrows(xp[niini], yp[niini], xp[nifin], yp[nifin],
-                      col = traj.colors[i], arr.adj = 1, lwd = traj.lwd[i], ...)
-      } else {shape::Arrows(xp[niini], yp[niini], xp[nifin], yp[nifin], arr.adj = 1)}
-
-    }
-  }
-}
-
