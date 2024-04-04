81df216a1ad9107d74525f08ac5fa3f1b42beddd
Martina Sánchez-Pinillos <martina.sanchezpinillos@gmail.com>
2024-04-04 17:41:52 -0400

    Include data for disturbed trajectories

--- R/EDR_data.R
+++ R/EDR_data.R
@@ -2,12 +2,12 @@
 #'
 #' @description
 #' Example datasets to characterize and compare EDRs, including abundance data,
-#' state, segment, and trajectory dissimilarity matrices for 90 artificial communities
+#' state, segment, and trajectory dissimilarity matrices for 93 artificial communities
 #' belonging to three different EDRs.
 #'
 #' @format
-#' List of three nested sublists (`"EDR1"`, `"EDR2"`, and `"EDR3"`), each
-#' associated with one EDR, including the following elements:
+#' List of four nested sublists. Each element of `"EDR1"`, `"EDR2"`, and `"EDR3"`
+#' is associated with one EDR and includes the following elements:
 #' * `abundance`: Data table with 15 columns and one row for each community state:
 #'     + `EDR`: Integer indicating the identifier of the EDR.
 #'     + `traj`: Integer containing the identifier of the trajectory for each
@@ -18,17 +18,28 @@
 #'     + `sp1, ..., sp12`: Vectors containing species abundances for each community
 #'     state.
 #' * `state_dissim`: Object of class `dist` containing Bray-Curtis dissimilarities
-#' between every pair of states in `abundance` (see Details).
+#' between every pair of states in `abundance`.
 #' * `segment_dissim`: Object of class `dist` containing the dissimilarities
-#' between every pair of trajectory segments in `abundance` (see Details).
+#' between every pair of trajectory segments in `abundance`.
 #' * `traj_dissim`: Object of class `dist` containing the dissimilarities
-#' between every pair of community trajectories in `abundance` (see Details).
+#' between every pair of community trajectories in `abundance`.
+#'
+#' The element `EDR3_disturbed` represents the dynamics of three disturbed communities
+#' originally associated with EDR3. It includes an abundance matrix with 16 columns
+#' and one row for each community state. The column `disturbed_states` is a numeric
+#' vector indicating whether the corresponding state represents a state before the
+#' disturbance (0), during or immediately after the release of the disturbance (1),
+#' or a post-disturbance state (> 1).
 #'
 #' @details
 #' Artificial data was generated following the procedure explained in Box 1 in
-#' Sánchez-Pinillos et al. (2023) In particular, the initial state of each community was
+#' Sánchez-Pinillos et al. (2023). The initial state of each community was
 #' defined using a hypothetical environmental space with optimal locations for
 #' 12 species. Community dynamics were simulated using a general Lotka-Volterra model.
+#'
+#' Abundances for `EDR3_disturbed` were generated following the procedure explained
+#' in Sánchez-Pinillos et al. (2024) for ecological systems affected by pulse
+#' disturbances.
 #'
 #' State dissimilarities were calculated using the Bray-Curtis metric. Segment and
 #' trajectory dissimilarities were calculated using the package 'ecotraj'.
@@ -37,6 +48,10 @@
 #' Sánchez-Pinillos, M., Kéfi, S., De Cáceres, M., Dakos, V. 2023. Ecological Dynamic
 #' Regimes: Identification, characterization, and comparison. *Ecological Monographs*.
 #' <doi:10.1002/ecm.1589>
+#'
+#' Sánchez-Pinillos, M., Dakos, V.,  Kéfi, S. 2024. Ecological Dynamic
+#' Regimes: A key concept for assessing ecological resilience. *Biological Conservation*.
+#' <doi:10.1016/j.biocon.2023.110409>
 #'
 #'
 "EDR_data"
