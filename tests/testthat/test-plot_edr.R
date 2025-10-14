36f849c20685185640459e66a7f3c3971c69a66b
Martina Sánchez-Pinillos <martina.sanchezpinillos@gmail.com>
2025-10-14 17:12:32 +0200

    Test that plots individual states. Add main text

--- tests/testthat/test-plot_edr.R
+++ tests/testthat/test-plot_edr.R
@@ -1,4 +1,5 @@
 test_that("works when trajectories and states are NOT in order (trajectories)", {
+  skip_on_cran()
   st1 <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4),
                     y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1),
                     traj = c(rep(1:3, each = 3), 3),
@@ -8,15 +9,16 @@ test_that("works when trajectories and states are NOT in order (trajectories)", 
 
   plot_edr(x = st1[, 1:2], trajectories = st1$traj, states = as.integer(st1$states),
            traj.colors = c("red", "green", "blue"), state.colors = NULL,
-           type = "trajectories")
+           type = "trajectories", main = 1)
 
   plot_edr(x = st2[, 1:2], trajectories = st2$traj, states = as.integer(st2$states),
            traj.colors = c( "blue", "red", "green"), state.colors = NULL,
-           type = "trajectories")
+           type = "trajectories", main = 2)
 
 })
 
 test_that("works when trajectories and states are NOT in order (states)", {
+  skip_on_cran()
   st1 <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4),
                     y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1),
                     traj = c(rep(1:3, each = 3), 3),
@@ -29,17 +31,18 @@ test_that("works when trajectories and states are NOT in order (states)", {
            traj.colors = c("red", "green", "blue"),
            state.colors = c("yellow", "orange", "red", "grey", "green", "darkgreen",
                             "pink", "purple", "blue", "black"),
-           type = "states", initial = TRUE)
+           type = "states", initial = TRUE, main = 1)
 
   plot_edr(x = st2[, 1:2], trajectories = st2$traj, states = as.integer(st2$states),
            traj.colors = c( "blue", "red", "green"),
            state.colors = c("purple", "red", "black", "yellow", "blue", "green",
                             "orange", "pink", "darkgreen", "grey"),
-           type = "states", initial = TRUE)
+           type = "states", initial = TRUE, main = 2)
 
 })
 
 test_that("works when trajectories and states are NOT in order (gradient)", {
+  skip_on_cran()
   st1 <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4),
                     y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1),
                     traj = c(rep(1:3, each = 3), 3),
@@ -50,15 +53,16 @@ test_that("works when trajectories and states are NOT in order (gradient)", {
 
   plot_edr(x = st1[, 1:2], trajectories = st1$traj, states = as.integer(st1$states),
            traj.colors = NULL, state.colors = viridis::viridis(3),
-           type = "gradient", variable = st1$x)
+           type = "gradient", variable = st1$x, main = 1)
 
   plot_edr(x = st2[, 1:2], trajectories = st2$traj, states = as.integer(st2$states),
            traj.colors = NULL, state.colors = viridis::viridis(3),
-           type = "gradient", variable = st2$x)
+           type = "gradient", variable = st2$x, main = 2)
 
 })
 
 test_that("works when x is dist", {
+  skip_on_cran()
   st <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4),
                    y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1),
                    traj = c(rep(1:3, each = 3), 3),
@@ -66,25 +70,37 @@ test_that("works when x is dist", {
 
   expect_warning(plot_edr(x = dist(st[, 1:2]), trajectories = st$traj, states = as.integer(st$states),
            traj.colors = c("red", "green", "blue"), state.colors = NULL,
-           type = "trajectories"))
+           type = "trajectories", main = "trajectories"))
 
   expect_warning(plot_edr(x = dist(st[, 1:2]), trajectories = st$traj, states = as.integer(st$states),
                           traj.colors = c("red", "green", "blue"),
                           state.colors = c("yellow", "orange", "red", "grey", "green", "darkgreen",
                                            "pink", "purple", "blue", "black"),
-                          type = "states"))
+                          type = "states", main = "states"))
   expect_warning(plot_edr(x = dist(st[, 1:2]), trajectories = st$traj, states = as.integer(st$states),
                           traj.colors = NULL, state.colors = viridis::viridis(3),
-                          type = "gradient", variable = st$x))
+                          type = "gradient", variable = st$x, main = "gradient"))
 })
 
 test_that("uses one color for all trajectories and states", {
+  skip_on_cran()
   st <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4),
                    y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1),
                    traj = c(rep(1:3, each = 3), 3),
                    states = c(rep(1:3, 3), 4))
   plot_edr(x = st[, 1:2], trajectories = st$traj, states = as.integer(st$states),
-           traj.colors = "blue", state.colors = "red",
+           traj.colors = "blue", state.colors = "red", initial = T,
+           type = "states")
+})
+
+test_that("plots individual states", {
+  skip_on_cran()
+  st <- data.frame(x = c(0, 1, 2, 1, 2, 3, 4, 4, 4, 4, 0.5),
+                   y = c(0, 0, 0, 1, 2, 3, 4, 3, 2, 1, 2),
+                   traj = c(rep(1:3, each = 3), 3, 4),
+                   states = c(rep(1:3, 3), 4, 1))
+  plot_edr(x = st[, 1:2], trajectories = st$traj, states = as.integer(st$states),
+           traj.colors = "blue", state.colors = c(rep("blue", 10), "red"),
            type = "states")
 })
 
